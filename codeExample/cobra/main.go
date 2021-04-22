0767eb555595ded9fb9671782e21395abf59bb86
MardyZhu <40788238+MardyZhu@users.noreply.github.com>
2021-04-22 18:59:30 +0800

    Add files via upload

---
+++ codeExample/cobra/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	// my cmd path
+
+	"git.ezbuy.me/ezbuy/zzztextzmd/cobra/cmd"
+)
+
+//  The main.go file serves one purpose:initializing Cobra.
+func main() {
+	cmd.Execute()
+}
